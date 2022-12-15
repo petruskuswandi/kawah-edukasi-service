3b168aaa24f8de05cd3f0e2494fe59de52c53f5a
RayRama24 <raihanramadhann@gmail.com>
2022-12-15 21:49:08 +0700

    handle max karacter deskripsi

--- src/main/java/id/kedukasi/core/models/Syillabus.java
+++ src/main/java/id/kedukasi/core/models/Syillabus.java
@@ -34,7 +34,7 @@ public class Syillabus {
     @Column(name="syllabus_name", nullable = false, length = 50)
     private String syillabusName;
 
-    @Column(name="description", nullable = false, columnDefinition = "text")
+    @Column(name="description", nullable = false, length = 500)
     private String description;
 
     @Column(name="isDeleted")
--- src/main/java/id/kedukasi/core/serviceImpl/SyillabusServiceImpl.java
+++ src/main/java/id/kedukasi/core/serviceImpl/SyillabusServiceImpl.java
@@ -47,8 +47,8 @@ public class SyillabusServiceImpl implements SyillabusService{
                 return ResponseEntity.badRequest().body(result);
             }
 
-            if(syillabus.getDescription().isBlank() || syillabus.getDescription().isEmpty()) {
-                result.setMessage("Error: Deskripsi syillabus tidak boleh kosong");
+            if(syillabus.getDescription().length()>500 || syillabus.getDescription().isBlank() || syillabus.getDescription().isEmpty()) {
+                result.setMessage("Error: Deskripsi syillabus tidak boleh kosong dan harus kurang dari 500 karakter");
                 result.setCode(HttpStatus.BAD_REQUEST.value());
                 return ResponseEntity.badRequest().body(result);
             }
@@ -82,8 +82,8 @@ public class SyillabusServiceImpl implements SyillabusService{
                 result.setCode(HttpStatus.BAD_REQUEST.value());
                 return ResponseEntity.badRequest().body(result);
             }
-            if (syillabus.getDescription().isBlank() || syillabus.getDescription().isEmpty()) {
-                result.setMessage("Error: Deskripsi syillabus tidak boleh kosong");
+            if (syillabus.getDescription().length()>500 || syillabus.getDescription().isBlank() || syillabus.getDescription().isEmpty()) {
+                result.setMessage("Error: Deskripsi syillabus tidak boleh kosong dan harus kurang dari 500 karakter");
                 result.setCode(HttpStatus.BAD_REQUEST.value());
                 return ResponseEntity.badRequest().body(result);
             }
