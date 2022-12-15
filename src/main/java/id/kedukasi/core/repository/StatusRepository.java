fc6ea3a2332b8bf91458cf093acdc5c39e8b9b51
Sirajuddin Hawari <sirajuddinhwr@gmail.com>
2022-12-15 13:08:32 +0700

    Menambahkan validasi unique dari kombinasi antara field statusName dan flag pada endpoint create dan update

--- src/main/java/id/kedukasi/core/repository/StatusRepository.java
+++ src/main/java/id/kedukasi/core/repository/StatusRepository.java
@@ -18,4 +18,35 @@ public interface StatusRepository extends JpaRepository<Status,Integer> {
     @Query("select count(*) as jumlah from Status as s where lower(s.statusName) = ?1")
     int findStatusname(String status_name);
 
+    /**
+     * Method untuk check kombinasi unique antara column statusName dan flag
+     * @param statusName
+     * @param flag
+     * @return
+     */
+    @Transactional
+    @Query("select count(*) as jumlah from Status as s where lower(s.statusName) = ?1 and lower(s.flag) = ?2")
+    int findStatusNameFlag(String statusName, String flag);
+
+    /**
+     * Method untuk check kombinasi unique antara column statusName dan flag khusus untuk update
+     * @param id
+     * @param statusName
+     * @param flag
+     * @return
+     */
+    @Transactional
+    @Query("select count(*) as jumlah from Status as s where s.id != ?1 and lower(s.statusName) = ?2 and lower(s.flag) = ?3")
+    int findUpdateStatusNameFlag(int id, String statusName, String flag);
+
+    /**
+     * Method untuk mencari id yang nilai statusName dan flag sama dengan parameter
+     * @param statusName
+     * @param flag
+     * @return
+     */
+    @Transactional
+    @Query("select s.id as identity from Status as s where lower(s.statusName) = ?1 and lower(s.flag) = ?2")
+    int findIdStatusByNameAndFlag(String statusName, String flag);
+
 }
