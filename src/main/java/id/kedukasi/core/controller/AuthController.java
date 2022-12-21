7716d99a976857678818478a60de4d792095e69b
Rizka Tauria Nuryadi <rizkataurian@gmail.com>
2022-12-21 22:23:27 +0700

    update AuthController: validasi harus akun sudah aktif untuk sign in, auto aktivasi akun pada sign up, dan validasi password lama pada patch change password

--- src/main/java/id/kedukasi/core/controller/AuthController.java
+++ src/main/java/id/kedukasi/core/controller/AuthController.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import id.kedukasi.core.request.ChangePasswordRequest;
 import id.kedukasi.core.request.LoginRequest;
 import id.kedukasi.core.models.Result;
+import id.kedukasi.core.repository.UserRepository;
 import id.kedukasi.core.request.SignupRequest;
 import id.kedukasi.core.request.TokenRefreshRequest;
 import id.kedukasi.core.service.UserService;
@@ -15,7 +16,9 @@ import io.swagger.annotations.ApiModelProperty;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -42,23 +45,66 @@ public class AuthController {
   @Autowired
   HttpServletRequest request;
 
+  @Autowired
+  UserRepository userRepository;
+
+  @Autowired
+  PasswordEncoder encoder;
+
   private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
   @PostMapping("/signin")
-  public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
+  public ResponseEntity<?> authenticateUser(
+          @Valid 
+          @RequestBody LoginRequest loginRequest) {
     String uri = stringUtil.getLogParam(request);
     logger.info(uri);
+    
+    Boolean isActive = userRepository.findByEmail(loginRequest.getEmail()).get().isIsActive();
 
+    Result result = new Result();
+    if(isActive.equals(false)){
+      result.setMessage("Akun Belum Aktif, Silahkan Aktivasi Akun Terlebih Dahulu untuk Dapat Sign In!");
+      result.setCode(HttpStatus.BAD_REQUEST.value());
+      return ResponseEntity
+              .badRequest()
+              .body(result);
+    }
+    
     return service.signIn(loginRequest, uri);
   }
 
   @PostMapping("/signup")
-  public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
-    return service.createUser(signUpRequest);
+  public ResponseEntity<?> registerUser(
+          @Valid 
+          @RequestBody SignupRequest signUpRequest) {//Password pada Signup Request di generate auto pada UserServiceImpl dan ikut disend pada email
+    service.createUser(signUpRequest); //Format Isian untuk memberikan id, token, email, dan password disamakan isiannya dengan Forgot Password di UserServiceImpl
+
+    Long id = userRepository.findByEmail(signUpRequest.getEmail()).get().getId();
+    String tokenVerification = userRepository.findByEmail(signUpRequest.getEmail()).get().getTokenVerification();
+    String statusAktif = "Aktif";
+
+    Result result = new Result();
+    if(statusAktif.equals("Aktif")){
+      String uri = stringUtil.getLogParam(request);
+      logger.info(uri);
+
+      service.active(id, tokenVerification, uri);
+
+      result.setMessage("User telah berhasil terdaftar dan akun sudah teraktivasi!");
+      result.setCode(HttpStatus.OK.value());
+      return ResponseEntity.ok(result);  
+    }
+
+    result.setMessage("User telah berhasil terdaftar, tetapi akun belum teraktivasi!");
+    result.setCode(HttpStatus.OK.value());
+    return ResponseEntity.ok(result);
   }
 
   @PostMapping("/signout")
-  public Result logout(@RequestParam(value = "id", defaultValue = "0", required = true) long id) {
+  public Result logout(
+          @RequestParam(value = "id", defaultValue = "0", required = true) 
+          long id) {
     String uri = stringUtil.getLogParam(request);
     logger.info(uri);
 
@@ -68,8 +114,8 @@ public class AuthController {
   @GetMapping("/active")
   public Result active(
           @RequestParam(value = "id", defaultValue = "0", required = true) long id,
-          @RequestParam(value = "tokenVerification", defaultValue = "", required = true) String tokenVerification
-  ) {
+          @RequestParam(value = "tokenVerification", defaultValue = "", required = true) 
+          String tokenVerification) {
     String uri = stringUtil.getLogParam(request);
     logger.info(uri);
 
@@ -79,21 +125,34 @@ public class AuthController {
   @PatchMapping("/change_password")
   public ResponseEntity<?> changePassword(
           @RequestParam(value = "id", defaultValue = "0", required = true) long id,
-          @RequestParam(value = "password", defaultValue = "", required = true) String password) {
+          @RequestParam(value = "password lama", defaultValue = "", required = true) String oldPassword,
+          @RequestParam(value = "password baru", defaultValue = "", required = true) String newPassword) {
     String uri = stringUtil.getLogParam(request);
     logger.info(uri);
 
-    return service.changePassword(id, password, uri);
+    String password = userRepository.findById(id).getPassword();
+
+    Result result = new Result();
+    if(!encoder.matches(oldPassword, password)){
+      result.setMessage("Password Lama yang Dimasukkan Tidak Sesuai");
+      result.setCode(HttpStatus.BAD_REQUEST.value());
+      return ResponseEntity
+              .badRequest()
+              .body(result);
+    }
+    return service.changePassword(id, newPassword, uri);
   }
 
   @PostMapping("/change_password_forgot")
-  public ResponseEntity<?> changePasswordForget(@RequestBody ChangePasswordRequest param) throws JsonProcessingException {
+  public ResponseEntity<?> changePasswordForget(
+          @RequestBody ChangePasswordRequest param) throws JsonProcessingException {
     return service.changePasswordForgot(param);
   }
 
   @PostMapping("/forgot_password")
   public ResponseEntity<?> forgotPassword(
-          @RequestParam(value = "email", defaultValue = "", required = true) String email) throws IOException {
+          @RequestParam(value = "email", defaultValue = "", required = true) 
+          String email) throws IOException {
     String uri = stringUtil.getLogParam(request);
     logger.info(uri);
 
@@ -101,7 +160,9 @@ public class AuthController {
   }
 
   @PostMapping("/refreshtoken")
-  public ResponseEntity<?> refreshtoken(@Valid @RequestBody TokenRefreshRequest request) {
+  public ResponseEntity<?> refreshtoken(
+          @Valid 
+          @RequestBody TokenRefreshRequest request) {
     return service.refreshToken(request);
   }
 }
