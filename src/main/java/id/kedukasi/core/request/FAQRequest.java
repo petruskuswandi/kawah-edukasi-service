bf8c388206f06e6207ce775520783b4a0626c0ad
Sirajuddin Hawari <sirajuddinhwr@gmail.com>
2023-01-17 17:41:09 +0700

    Merapihkan dan menambahkan sedikit fitur pada service FAQ dan Status

--- src/main/java/id/kedukasi/core/request/FAQRequest.java
+++ src/main/java/id/kedukasi/core/request/FAQRequest.java
@@ -2,32 +2,36 @@ package id.kedukasi.core.request;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.Size;
+
 import io.swagger.annotations.ApiModelProperty;
 
 public class FAQRequest implements Serializable {
     
     /* Properties Input in Swagger-UI */
-    @ApiModelProperty(example = "Apakah ini bisa diisi dengan pertanyaan?", required = true)
+    @Size(max = 500)
+    @ApiModelProperty(example = "Apakah ini bisa diisi dengan pertanyaan?", required = true, position = 1)
     private String question;
     
-    @ApiModelProperty(example = "Tidak, di sini hanya berisi jawaban", required = true)
+    @Size(max = 500)
+    @ApiModelProperty(example = "Tidak, di sini hanya berisi jawaban", required = true, position = 2)
     private String answer;
 
     /* Method of propeties */
-    public String getAnswer() {
-        return answer;
-    }
-
-    public void setAnswer(String answer) {
-        this.answer = answer;
-    }
-
     public String getQuestion() {
         return question;
     }
 
     public void setQuestion(String question) {
         this.question = question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
     }
     
 }
