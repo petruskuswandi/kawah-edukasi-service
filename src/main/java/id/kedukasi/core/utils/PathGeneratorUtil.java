a9c96485b9129d3ea505843a2d3df02b52932235
Yudha Kirana <yudhkra212@gmail.com>
2023-01-19 11:05:54 +0700

    path preview otomatis jila file gambar

--- src/main/java/id/kedukasi/core/utils/PathGeneratorUtil.java
+++ src/main/java/id/kedukasi/core/utils/PathGeneratorUtil.java
@@ -7,9 +7,14 @@ public class PathGeneratorUtil {
 
     public static String generate(String fileCode, HttpServletRequest request) {
 
+        String fileType = fileCode.substring(0, 3);
         String hostName = request.getServerName();
         int port = request.getServerPort();
         String schema = request.getScheme();
+
+        if (fileType.equals("IMG")) {
+            return schema + "://" + hostName + ":" + port + "/previewFile/utility/" + fileCode;
+        }
 
         return schema + "://" + hostName + ":" + port + "/downloadFile/utility/" + fileCode;
     }
