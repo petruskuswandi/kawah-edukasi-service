59a32fa12e3af0b036c08caadd3c9d600562636b
Petrus Kuswandi <petruskuswandi1@gmail.com>
2023-07-28 22:04:23 +0700

    First commit project kawah-edukasi

--- src/main/java/id/kedukasi/core/controller/BatchController.java
+++ src/main/java/id/kedukasi/core/controller/BatchController.java
@@ -1,7 +1,6 @@
 package id.kedukasi.core.controller;
 
 import id.kedukasi.core.models.Result;
-import id.kedukasi.core.models.User;
 import id.kedukasi.core.request.BatchRequest;
 import id.kedukasi.core.request.CreateBatchRequest;
 import id.kedukasi.core.service.BatchService;
@@ -10,12 +9,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
-
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @CrossOrigin
